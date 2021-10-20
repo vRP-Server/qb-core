299ab781f94dc9389f9052c21fcad829c9def7cc
WRIGH7Y <92814197+WRIGH7Y@users.noreply.github.com>
2021-10-20 18:49:41 +0100

    Started using GitHub
    Started using GitHub

--- client/functions.lua
+++ client/functions.lua
@@ -623,3 +623,29 @@ function QBCore.Functions.SetVehicleProperties(vehicle, props)
         end
     end
 end
+--||||||||||||||||||||||FOR MECHANIC JOB|||||||||||||||||||||||||||||||||||||||
+
+QBCore.Functions.MathRound = function(value, numDecimalPlaces)
+    if numDecimalPlaces then
+        local power = 10^numDecimalPlaces
+        return math.floor((value * power) + 0.5) / (power)
+    else
+        return math.floor(value + 0.5)
+    end
+end
+
+QBCore.Functions.MathGroupDigits = function(value)
+    local left,num,right = string.match(value,'^([^%d]*%d)(%d*)(.-)$')
+
+    return left..(num:reverse():gsub('(%d%d%d)','%1' .. _U('locale_digit_grouping_symbol')):reverse())..right
+end
+
+QBCore.Functions.MathTrim = function(value)
+    if value then
+        return (string.gsub(value, "^%s*(.-)%s*$", "%1"))
+    else
+        return nil
+    end
+end
+
+--|||||||||||||||||||||||FOR MECHANIC JOB|||||||||||||||||||||||||||||||||||||||
--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 QBConfig = {}
 
-QBConfig.MaxPlayers = GetConvarInt('sv_maxclients', 64) -- Gets max players from config file, default 32
+QBConfig.MaxPlayers = GetConvarInt('sv_maxclients', 48) -- Gets max players from config file, default 32
 QBConfig.DefaultSpawn = vector4(-1035.71, -2731.87, 12.86, 0.0)
 QBConfig.UpdateInterval = 5 -- how often to update player data in minutes
 QBConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in ms
@@ -8,10 +8,10 @@ QBConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in ms
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = { ['cash'] = 500, ['bank'] = 5000, ['crypto'] = 0 } -- ['type']=startamount - Add or remove money types for your server (for ex. ['blackmoney']=0), remember once added it will not be removed from the database!
 QBConfig.Money.DontAllowMinus = { 'cash', 'crypto' } -- Money that is not allowed going in minus
-QBConfig.Money.PayCheckTimeOut = 10 -- The time in minutes that it will give the paycheck
+QBConfig.Money.PayCheckTimeOut = 20 -- The time in minutes that it will give the paycheck
 
 QBConfig.Player = {}
-QBConfig.Player.MaxWeight = 120000 -- Max weight a player can carry (currently 120kg, written in grams)
+QBConfig.Player.MaxWeight = 150000 -- Max weight a player can carry (currently 120kg, written in grams)
 QBConfig.Player.MaxInvSlots = 41 -- Max inventory slots for a player
 QBConfig.Player.HungerRate = 4.2 -- Rate at which hunger goes down.
 QBConfig.Player.ThirstRate = 3.8 -- Rate at which thirst goes down.
@@ -38,7 +38,7 @@ QBConfig.Notify = {}
 
 QBConfig.Notify.NotificationStyling = {
     group = false, -- Allow notifications to stack with a badge instead of repeating
-    position = "right", -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
+    position = "left", -- top-left | top-right | bottom-left | bottom-right | top | bottom | left | right | center
     progress = true -- Display Progress Bar
 }
 
